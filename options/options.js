dec8d95819aa39191f56ff7ed65bcb90118920a8
Crowdbotics <team@crowdbotics.com>
2023-06-19 01:14:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "light-upon-light-41583",
+  url: "https://light-upon-light-41583.botics.co",
+  api: "https://light-upon-light-41583.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
